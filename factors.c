ffe12517f71ad58d27713b8b0edc0f4fa418f43a
kassem00 <kamsdonga@gmail.com>
2023-09-01 12:08:30 +0300

    try withbug

---
+++ factors.c
@@ -0,0 +1,45 @@
+#include "lib.h"
+#define BUF_SIZE 1024
+/**
+ * main - Factorize as many numbers as possible
+ * @argc: Number of arguments
+ * @argv: Pointer to arguments
+ * Return: int value
+ */
+int main(int argc, char **argv)
+{
+	FILE *fip;
+	char *buf = calloc(BUF_SIZE, sizeof(char));
+	size_t s_buf = 255;
+	ssize_t lines = 0;
+	if (argc != 2)
+		exit(EXIT_FAILURE);
+	fip = fopen(argv[1], "r");
+	if (fip == NULL)
+		exit(EXIT_FAILURE);
+	lines = getline(&buf, &s_buf, fip);
+	while (lines != -1)
+	{
+		factor(buf);
+		lines = getline(&buf, &s_buf, fip);
+	}
+
+	return (0);
+}
+/**
+ * factor - Factorize as many numbers as possible
+ * @buf: string to be converted to number
+ * Return: void
+ */
+void factor(char *buf)
+{
+	u_int32_t num = atoi(buf), i = 2;
+	while (i < num)
+	{
+		if (num % i == 0)
+			break;
+		else
+			i++;
+	}
+	printf("%d=%d*%d\n",num, num / i, i);
+}
