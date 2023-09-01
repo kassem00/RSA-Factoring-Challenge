74469115605e1d02cab1583fff740074aa5cc7a1
kassem00 <kamsdonga@gmail.com>
2023-09-01 12:22:37 +0300

    e

--- factors.c
+++ factors.c
@@ -1,22 +1,20 @@
 #include "lib.h"
 #define BUF_SIZE 1024
-/**
- * main - Factorize as many numbers as possible
- * @argc: Number of arguments
- * @argv: Pointer to arguments
- * Return: int value
- */
+
 int main(int argc, char **argv)
 {
 	FILE *fip;
 	char *buf = calloc(BUF_SIZE, sizeof(char));
 	size_t s_buf = 255;
 	ssize_t lines = 0;
+
 	if (argc != 2)
 		exit(EXIT_FAILURE);
+
 	fip = fopen(argv[1], "r");
 	if (fip == NULL)
 		exit(EXIT_FAILURE);
+
 	lines = getline(&buf, &s_buf, fip);
 	while (lines != -1)
 	{
@@ -24,22 +22,22 @@ int main(int argc, char **argv)
 		lines = getline(&buf, &s_buf, fip);
 	}
 
-	return (0);
+	return 0;
 }
-/**
- * factor - Factorize as many numbers as possible
- * @buf: string to be converted to number
- * Return: void
- */
+
 void factor(char *buf)
 {
-	u_int32_t num = atoi(buf), i = 2;
-	while (i < num)
+	unsigned long long int num = atoll(buf);
+	unsigned long long int i = 2;
+	int x = 0;
+
+	while (i <= num)
 	{
 		if (num % i == 0)
 			break;
 		else
 			i++;
 	}
-	printf("%d=%d*%d\n",num, num / i, i);
+
+	printf("%llu=%llu*%llu\n", num, num / i, i);
 }
